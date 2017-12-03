d969d8d01a350486a2790fcb89cb16f33e5dc75e
NathansScripts <nathan.numbers@gmail.com>
2017-12-03 03:39:28 -0500

    Added test player names

--- code/test.py
+++ code/test.py
@@ -1,5 +1,7 @@
 #To test adding players and 
 
+import PodSixNet.Channel
+import PodSixNet.Server
 #from client.server import *
 #import client.game_menu_ui
 from game import Game
@@ -8,11 +10,13 @@ from card_controller import Card
 from location import Location
 from player import Player
 #from client.server import ClientChannel
-
+import compileall
 import threading
 import time
 from subprocess import check_output, call
 
+serverFileName = "client/server.py"
+clientFileName = "client/game_menu_ui.py"
 testPlayers=[]
 
 testPlayers.append("Michael")
@@ -20,32 +24,36 @@ testPlayers.append("Alice")
 testPlayers.append("Peter")
 testPlayers.append("Hercules")
 testPlayersCount = len(testPlayers)
+myPyFile = "client/server.py"
 
 def startServer():
     print(threading.currentThread().getName(), 'Started')
-    time.sleep(1)
-    #execfile('./client/server.py')
-    exec(open("./client/server.py").read())
-    exec(open("./client/server.py").read())
+    compiled = compile('',serverFileName, 'exec')
+    exec(compiled)
+    time.sleep(10)
+    #exec(open(myPyFile).read())
+    #exec(open("./client/server.py").read())
     #call('python3.4 /server.py')
     print(threading.currentThread().getName(), 'Ended')
     
           
 def startBoardGame():
     print(threading.currentThread().getName(), 'joined the game')
-    #exec("game_menu_ui.py")
+    compiled = compile('',clientFileName, 'exec')
+    exec(compiled)
     time.sleep(2)
     print(threading.currentThread().getName(), 'left the game')
 
 TestServer = threading.Thread(name='Server', target=startServer)
 TestServer.setDaemon(True)
 TestServer.start()
-time.sleep(5)
+time.sleep(1)
 
 for i in testPlayers:
     i = threading.Thread(name=i, target=startBoardGame)
     i.setDaemon(True)
     i.start()
+
 #for i in testPlayers:
 #    i.join()
 #    print(i)
