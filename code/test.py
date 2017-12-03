b166e8573815d940f4ff8fe31f85925835086032
NathansScripts <nathan.numbers@gmail.com>
2017-12-03 02:53:13 -0500

    Added threads to simulate players.

--- code/test.py
+++ code/test.py
@@ -1,27 +1,59 @@
 #To test adding players and 
 
-#assume exactly 2 players.
-
+#from client.server import *
+#import client.game_menu_ui
 from game import Game
+from card_controller import CardController
+from card_controller import Card
+from location import Location
+from player import Player
+#from client.server import ClientChannel
 
-thisGame = Game()
+import threading
+import time
+from subprocess import check_output, call
 
-#for l in thisGame.locations:
-    #print(l.isRoom)
+testPlayers=[]
+
+testPlayers.append("Michael")
+testPlayers.append("Alice")
+testPlayers.append("Peter")
+testPlayers.append("Hercules")
+testPlayersCount = len(testPlayers)
+
+def startServer():
+    print(threading.currentThread().getName(), 'Started')
+    time.sleep(1)
+    #execfile('./client/server.py')
+    exec(open("./client/server.py").read())
+    exec(open("./client/server.py").read())
+    #call('python3.4 /server.py')
+    print(threading.currentThread().getName(), 'Ended')
+    
+          
+def startBoardGame():
+    print(threading.currentThread().getName(), 'joined the game')
+    #exec("game_menu_ui.py")
+    time.sleep(2)
+    print(threading.currentThread().getName(), 'left the game')
+
+TestServer = threading.Thread(name='Server', target=startServer)
+TestServer.setDaemon(True)
+TestServer.start()
+time.sleep(5)
+
+for i in testPlayers:
+    i = threading.Thread(name=i, target=startBoardGame)
+    i.setDaemon(True)
+    i.start()
+#for i in testPlayers:
+#    i.join()
+#    print(i)
+    
+#for thread in threading.enumerate():
+    #thread.join()
+    #print(thread.name)    
+
+TestServer.join()
         
-me1 = thisGame.addPlayer()
-
-print(me1.name)
-
-me2 = thisGame.addPlayer()
-
-print(me2.name)
-
-me3 = thisGame.addPlayer()
-
-print(me3.name)
-
-
-print(me3.getLocation().name)
-thisGame.makeMove(me3, thisGame.locations[0])
-print(me3.getLocation().name)
+print("\nTest End")
