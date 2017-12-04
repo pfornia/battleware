23cf2c0214353f5891fdb781d869018e222993d2
NathansScripts <nathan.numbers@gmail.com>
2017-12-03 21:55:33 -0500

    Adding Killing threads.

--- code/test.py
+++ code/test.py
@@ -11,7 +11,7 @@ import threading
 import time
 from subprocess import check_output, call, Popen
 
-serverFileName = "client/server.py"
+serverFileName = "server.py"
 clientFileName = "client/game_menu_ui.py"
 testPlayers=[]
 
@@ -22,25 +22,25 @@ testPlayers.append("Hercules")
 testPlayersCount = len(testPlayers)
 
 def startServer():
-    print(threading.currentThread().getName(), 'Started')
+    print(threading.currentThread().getName(), 'Executing Server')
     #compiled = compile('',serverFileName, 'exec')
     #exec(compiled)
     
     call(["python3.4",serverFileName])
-    time.sleep(10)
+    time.sleep(2)
     #exec(open(myPyFile).read())
     #exec(open("./client/server.py").read())
     #call('python3.4 /server.py')
-    print(threading.currentThread().getName(), 'Ended')
+    print(threading.currentThread().getName(), 'Closing Server')
     
           
 def startBoardGame():
-    print(threading.currentThread().getName(), 'joined the game')
+    print(threading.currentThread().getName(), 'Executing a client session')
     call(["python3.4",clientFileName])
     time.sleep(2)
-    print(threading.currentThread().getName(), 'left the game')
+    print(threading.currentThread().getName(), 'Closed a client session')
 
-TestServer = threading.Thread(name='Server', target=startServer)
+TestServer = threading.Thread(name='', target=startServer)
 TestServer.setDaemon(True)
 TestServer.start()
 time.sleep(1)
@@ -53,11 +53,12 @@ for i in testPlayers:
 #for i in testPlayers:
 #    i.join()
 #    print(i)
-    
+i.join()    
 #for thread in threading.enumerate():
     #thread.join()
     #print(thread.name)    
+time.sleep(5)
 
-TestServer.join()
+#TestServer.join()
         
 print("\nTest End")
